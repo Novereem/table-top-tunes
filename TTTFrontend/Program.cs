b9d616d276718b47867275b5300e2944a6432f00
Overeem,Noah N.V. <n.overeem@student.fontys.nl>
2024-09-17 19:10:45 +0200

    Add project files.

---
+++ TTTFrontend/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using TTTFrontend;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<WeatherService>();
+
+builder.Services.AddScoped(sp => new HttpClient { 
+    BaseAddress = new Uri( /*replace this with the address your api will be hosted*/ "https://localhost:7041")
+});
+
+await builder.Build().RunAsync();
