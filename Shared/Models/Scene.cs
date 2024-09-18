e202c82db00a242bd3cf9d0366d2ea606110ebe5
Overeem,Noah N.V. <n.overeem@student.fontys.nl>
2024-09-18 22:57:00 +0200

    Set up folder and component structure, added scenes in frontend

---
+++ Shared/Models/Scene.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Scene
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public Scene(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+        }
+    }
+}
