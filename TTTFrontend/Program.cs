5fd7148b46076118ed9136672afda665b95394c9
Noah Overeem <noovereem@gmail.com>
2024-11-20 16:16:41 +0100

    succesful login system storing jtw token

--- TTTFrontend/Program.cs
+++ TTTFrontend/Program.cs
@@ -1,6 +1,8 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TTTFrontend;
+using TTTFrontend.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +11,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { 
     BaseAddress = new Uri( /*replace this with the address your api will be hosted*/ "https://localhost:7041")
 });
+builder.Services.AddScoped<AuthenticationService>();
+builder.Services.AddBlazoredLocalStorage();
+
 
 await builder.Build().RunAsync();
---
+++ TTTFrontend/Services/AuthenticationService.cs
@@ -0,0 +1,59 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using Shared.Models.DTOs;
+using System.Net.Http.Json;
+
+namespace TTTFrontend.Services
+{
+    public class AuthenticationService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ISyncLocalStorageService _localStorage;
+        private readonly NavigationManager _navigationManager;
+
+        public AuthenticationService(HttpClient httpClient, ISyncLocalStorageService localStorage, NavigationManager navigationManager)
+        {
+            _httpClient = httpClient;
+            _localStorage = localStorage;
+            _navigationManager = navigationManager;
+        }
+
+        public async Task<LoginResponseDTO> Login(UserLoginDTO loginModel)
+        {
+            var response = await _httpClient.PostAsJsonAsync("https://localhost:7041/api/Authentication/login", loginModel);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseData = await response.Content.ReadFromJsonAsync<LoginResponseDTO>();
+                if (responseData != null && !string.IsNullOrEmpty(responseData.Token))
+                {
+                    // Return success response with token
+                    return new LoginResponseDTO
+                    {
+                        Success = true,
+                        Token = responseData.Token
+                    };
+                }
+            }
+
+            // If the login was unsuccessful, capture the error message
+            var errorMessage = await response.Content.ReadAsStringAsync();
+            return new LoginResponseDTO
+            {
+                Success = false,
+                ErrorMessage = errorMessage
+            };
+        }
+
+        public void Logout()
+        {
+            _localStorage.RemoveItem("authToken");
+            _navigationManager.NavigateTo("/login");
+        }
+
+        public string GetToken()
+        {
+            return _localStorage.GetItem<string>("authToken");
+        }
+    }
+}
